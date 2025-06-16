3797c3cb9b507f2095f9de7b493976648c95e01b
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-06-16 12:15:25 -0300

    NodeJS, Express, MySQL, MongoDB

---
+++ gdp-VitorLima/blogapp/app.js
@@ -0,0 +1,33 @@
+// Carregando módulos
+const express = require("express")
+const { engine } = require("express-handlebars")
+const bodyParser = require("body-parser")
+const app = express()
+const admin = require("./routes/admin")
+const path =require("path")
+//const mongoose = require("mongoose")
+
+// Configurações
+    // Body Parser
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+
+    // Handlebars
+    app.engine("handlebars", engine({defaultLayout: 'main'}))
+    app.set("view engine", "handlebars")
+
+    // Mongoose
+        // em breve
+
+    // Public
+    app.use(express.static(path.join(__dirname, "public")))
+
+// Rotas
+app.use("/admin", admin)
+
+
+// Outros
+const PORT = 8081
+app.listen(PORT, () => {
+    console.log(`Servidor Rodando! http://localhost:${PORT}`)
+})
---
+++ gdp-VitorLima/blogapp/routes/admin.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.render("admin/index")
+})
+
+router.get("/posts", (req, res) => {
+    res.send("Posts")
+})
+
+router.get("/categorias", (req, res) => {
+    res.render("admin/categorias")
+})
+
+router.get("/categorias/add", (req, res) => {
+    res.render("admin/addcategoria")
+})
+
+module.exports = router
