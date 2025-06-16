9e5e820d1263322025375ead4114e3a146853071
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-06-16 17:16:46 -0300

    NodeJS, Express, MySQL, MongoDB

--- gdp-VitorLima/blogapp/app.js
+++ gdp-VitorLima/blogapp/app.js
@@ -5,9 +5,26 @@ const bodyParser = require("body-parser")
 const app = express()
 const admin = require("./routes/admin")
 const path =require("path")
-//const mongoose = require("mongoose")
+const mongoose = require("mongoose")
+const session = require("express-session")
+const flash = require("connect-flash")
 
 // Configurações
+    // Session
+    app.use(session({
+        secret: "cursodenode",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+
+    // Middleware
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+    })
+
     // Body Parser
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
@@ -17,7 +34,12 @@ const path =require("path")
     app.set("view engine", "handlebars")
 
     // Mongoose
-        // em breve
+    mongoose.Promise = global.Promise
+    mongoose.connect("mongodb://localhost/blogapp").then(() => {
+        console.log("Conectado ao MongoDB!")
+    }).catch((err) => {
+        console.log(`Erro ao se conectar com o MongoDB: ${err}`)
+    })
 
     // Public
     app.use(express.static(path.join(__dirname, "public")))
@@ -29,5 +51,5 @@ app.use("/admin", admin)
 // Outros
 const PORT = 8081
 app.listen(PORT, () => {
-    console.log(`Servidor Rodando! http://localhost:${PORT}`)
+    console.log(`Servidor Rodando! http://localhost:${PORT}/admin/categorias`)
 })
