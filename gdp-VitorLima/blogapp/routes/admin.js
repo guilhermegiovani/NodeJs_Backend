b6bd8abd31b59fbfdb2898927518d5d43fd93bd0
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-06-17 12:06:00 -0300

    Blog app

--- gdp-VitorLima/blogapp/routes/admin.js
+++ gdp-VitorLima/blogapp/routes/admin.js
@@ -4,6 +4,28 @@ const mongoose = require("mongoose")
 require("../models/Categoria")
 const Categoria = mongoose.model("categorias")
 
+function validarCategoria(body) {
+    const erros = []
+
+    const nome = body.nome.trim()
+    const slug = body.slug.trim()
+
+
+    if (!nome || typeof nome == undefined || nome == null) {
+        erros.push({ texto: "Nome inválido!" })
+    }
+
+    if (!slug || typeof slug == undefined || slug == null) {
+        erros.push({ texto: "Slug inválido!" })
+    }
+
+    if (nome.length < 2) {
+        erros.push({ texto: "Nome da categoria muito pequeno!" })
+    }
+
+    return erros
+}
+
 router.get("/", (req, res) => {
     res.render("admin/index")
 })
@@ -13,7 +35,12 @@ router.get("/posts", (req, res) => {
 })
 
 router.get("/categorias", (req, res) => {
-    res.render("admin/categorias")
+    Categoria.find().lean().sort({ date: 'desc' }).then((categorias) => {
+        res.render("admin/categorias", { categorias: categorias })
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao listar as categorias!")
+        res.redirect("/admin")
+    })
 })
 
 router.get("/categorias/add", (req, res) => {
@@ -21,15 +48,75 @@ router.get("/categorias/add", (req, res) => {
 })
 
 router.post("/categorias/nova", (req, res) => {
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+
+    const erros = validarCategoria(req.body)
+
+    if (erros.length > 0) {
+        const mensagens = erros.map(e => e.texto).join(" | ")
+        req.flash("error_msg", mensagens)
+        res.redirect("/admin/categorias")
+    } else {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash("success_msg", "Categoria criada com sucesso!")
+            res.redirect("/admin/categorias")
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamente!")
+            res.redirect("/admin/categorias")
+            // console.log(`Erro ao salvar categoria: ${err}`)
+        })
     }
 
-    new Categoria(novaCategoria).save().then(() => {
-        console.log("Categoria salva com sucesso!")
+})
+
+router.get("/categorias/edit/:id", (req, res) => {
+    Categoria.findOne({ _id: req.params.id }).lean().then((categoria) => {
+        res.render("admin/editcategoria", { categoria: categoria })
     }).catch((err) => {
-        console.log(`Erro ao salvar categoria: ${err}`)
+        req.flash("error_msg", `Esta categoria não existe: ${err}`)
+        res.redirect("/admin/categorias")
+    })
+})
+
+router.post("/categorias/edit", (req, res) => {
+    Categoria.findOne({ _id: req.body.id }).then((categoria) => {
+
+        const erros = validarCategoria(req.body)
+
+        if (erros.length > 0) {
+            const mensagens = erros.map(e => e.texto).join(" | ")
+            req.flash("error_msg", mensagens)
+            res.redirect("/admin/categorias")
+        } else {
+            categoria.nome = req.body.nome
+            categoria.slug = req.body.slug
+
+            categoria.save().then(() => {
+                req.flash("success_msg", "Categoria editada com sucesso!")
+                res.redirect("/admin/categorias")
+            }).catch((err) => {
+                req.flash("error_msg", `Houve um erro interno ao salvar a edição da categoria: ${err}`)
+                res.redirect("/admin/categorias")
+            })
+        }
+
+    }).catch((err) => {
+        req.flash("error_msg", `Houve um erro ao editar a categoria: ${err}`)
+        res.redirect("/admin/categorias")
+    })
+})
+
+router.post("/categorias/deletar", (req, res) => {
+    Categoria.deleteOne({_id: req.body.id}).then(() => {
+        req.flash("success_msg", "Categoria deletada com sucesso!")
+        res.redirect("/admin/categorias")
+    }).catch((err) => {
+        req.flash("error_msg", `Houve um erro ao deletar categoria: ${err}`)
+        res.redirect("/admin/categorias")
     })
 })
 
